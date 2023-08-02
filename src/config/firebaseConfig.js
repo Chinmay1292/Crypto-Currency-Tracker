255c7d5b8f414974d2d12a2e7a53a6fbe84b4233
Chinmay1292 <chinmaychougule1212@gmail.com>
2023-08-02 17:03:53 +0530

    firebase and auth

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: process.env.api,
+    authDomain: process.env.authD,
+    projectId: process.env.pro,
+    storageBucket: process.env.sto,
+    messagingSenderId: process.env.mes,
+    appId: process.env.app,
+  };
+  
+  export default firebaseConfig;
